3ccc28dffe88ee2140dc68e1ba521e9659034016
ruddnjs3769 <84277185+ruddnjs3769@users.noreply.github.com>
2023-07-25 16:53:32 +0900

    feat : alias설정, variables, react-router분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import "./App.css";
+import './App.module.scss'
+import { Route, Routes } from 'react-router-dom'
+import MainLayout from '@/layout/MainLayout'
+import Main from '@/pages/main/Main'
+import Mypage from '@/pages/mypage/Mypage'
 
 function App() {
-  return <div>Hello!</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route path="/" element={<Main />} />
+        <Route path="/mypage" element={<Mypage />} />
+      </Route>
+    </Routes>
+  )
 }
 
-export default App;
+export default App
