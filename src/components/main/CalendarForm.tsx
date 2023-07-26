7867f5d653d3f2dc856dedfed017b82dc3557225
ruddnjs3769 <84277185+ruddnjs3769@users.noreply.github.com>
2023-07-26 16:57:25 +0900

    [#1] CalendarForm에 DummyAllAnnual 출력

--- src/components/main/CalendarForm.tsx
+++ src/components/main/CalendarForm.tsx
@@ -1,44 +1,33 @@
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from '@fullcalendar/interaction'
-import { DateSelectArg, EventClickArg, EventContentArg, EventApi } from '@fullcalendar/core'
 import style from './CalendarForm.module.scss'
 import { useState, useEffect } from 'react'
 import DummyAllAnnual from './DummyAllAnnual.json'
-import { createEventId } from '@/utils/CalendarUtils'
+
+interface EventObject {
+  title: string
+  date: string
+}
 
 const CalendarForm = () => {
-  const [currentEvents, setCurrentEvents] = useState<EventApi[]>([])
-  const handleDateSelect = (selectInfo: DateSelectArg) => {
-    console.log(selectInfo)
-    const title = prompt('연차를 등록해주세요: 이름') // 차후 유저 구현 시 이름으로 대체
-    const calendarApi = selectInfo.view.calendar
+  const [currentEvents, setCurrentEvents] = useState<EventObject[]>([])
 
-    calendarApi.unselect() // clear date selection
-
-    if (title) {
-      calendarApi.addEvent({
-        id: createEventId(), // annualId로 대체
-        title,
-        start: selectInfo.startStr,
-        end: selectInfo.endStr,
-        allDay: selectInfo.allDay
-      })
+  useEffect(() => {
+    const events = []
+    const month = 7 // assuming July
+    for (const [day, data] of Object.entries(DummyAllAnnual.data)) {
+      for (const item of data) {
+        events.push({
+          title: item.name + '#' + item.employeeNumber.slice(0, 3),
+          date: new Date(2023, month - 1, Number(day)).toISOString().split('T')[0]
+        })
+      }
     }
-  }
-
-  const renderEventContent = (eventContent: EventContentArg) => {
-    return (
-      <>
-        <i>{eventContent.event.title}</i>
-      </>
-    )
-  }
-
-  const handleEvents = (events: EventApi[]) => {
     setCurrentEvents(events)
-    console.log('handleEvents', events)
-  }
+    console.log(events)
+  }, [])
+
   return (
     <>
       <FullCalendar
@@ -50,10 +39,10 @@ const CalendarForm = () => {
         }}
         editable={true}
         selectable={true}
-        select={handleDateSelect}
+        // select={handleDateSelect}
         selectMirror={false}
-        eventContent={renderEventContent}
-        eventsSet={handleEvents}
+        events={currentEvents}
+        locale="ko"
       />
     </>
   )
