9d1d2aa02206b005b6af452e78fe780517f96640
ruddnjs3769 <84277185+ruddnjs3769@users.noreply.github.com>
2023-07-26 17:28:10 +0900

    [#1] feat : dummy8월 추가

--- src/components/main/CalendarForm.tsx
+++ src/components/main/CalendarForm.tsx
@@ -1,9 +1,11 @@
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from '@fullcalendar/interaction'
-import style from './CalendarForm.module.scss'
-import { useState, useEffect } from 'react'
-import DummyAllAnnual from './DummyAllAnnual.json'
+// import style from './CalendarForm.module.scss'
+import { useState, useEffect, useRef } from 'react'
+import DummyAllAnnual7 from './DummyAllAnnual7.json'
+import DummyAllAnnual8 from './DummyAllAnnual8.json'
+import _ from 'lodash'
 
 interface EventObject {
   title: string
@@ -11,12 +13,20 @@ interface EventObject {
 }
 
 const CalendarForm = () => {
+  const date = new Date()
+  const initialYear = date.getFullYear()
+  const initialMonth = date.getMonth() + 1
   const [currentEvents, setCurrentEvents] = useState<EventObject[]>([])
+  // 캘린더 이전, 다음달 변경 시 년/월 정보
+  const [year, setYear] = useState(initialYear)
+  const [month, setMonth] = useState(initialMonth)
+  // 캘린더 정보
+  const calendarRef = useRef<FullCalendar>(null)
 
   useEffect(() => {
     const events = []
     const month = 7 // assuming July
-    for (const [day, data] of Object.entries(DummyAllAnnual.data)) {
+    for (const [day, data] of Object.entries(DummyAllAnnual7.data)) {
       for (const item of data) {
         events.push({
           title: item.name + '#' + item.employeeNumber.slice(0, 3),
@@ -31,11 +41,13 @@ const CalendarForm = () => {
   return (
     <>
       <FullCalendar
+        ref={calendarRef}
         initialView="dayGridMonth"
         plugins={[dayGridPlugin, interactionPlugin]}
         headerToolbar={{
-          left: 'prev,next today',
-          center: 'title'
+          start: '',
+          center: 'title',
+          end: 'today prev next'
         }}
         editable={true}
         selectable={true}
@@ -43,6 +55,17 @@ const CalendarForm = () => {
         selectMirror={false}
         events={currentEvents}
         locale="ko"
+        customButtons={{
+          prev: {
+            icon: 'chevron-left',
+            click: () => {
+              if (calendarRef.current?.getApi()) {
+                calendarRef.current.getApi().prev()
+                const calendarMonth = _.get
+              }
+            }
+          }
+        }}
       />
     </>
   )
