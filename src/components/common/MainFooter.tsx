9b2e327e8b840d6fc422ca28bd98d42ab7048bc4
ruddnjs3769 <84277185+ruddnjs3769@users.noreply.github.com>
2023-07-26 13:24:19 +0900

    [#1] feat: Header, Footer, Calender 컴포넌트 추가(기능구현X)

---
+++ src/components/common/MainFooter.tsx
@@ -0,0 +1,7 @@
+import style from './MainFooter.module.scss'
+
+const MainFooter = () => {
+  return <div>MainFooter</div>
+}
+
+export default MainFooter
---
+++ src/components/common/MainHeader.tsx
@@ -0,0 +1,7 @@
+import style from 'MainHeader.module.scss'
+
+const MainHeader = () => {
+  return <div>MainHeader</div>
+}
+
+export default MainHeader
