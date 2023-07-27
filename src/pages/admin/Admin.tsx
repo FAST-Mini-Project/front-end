e9b120f12d42828ed6279d42f97929a6104a0819
ruddnjs3769 <84277185+ruddnjs3769@users.noreply.github.com>
2023-07-27 14:12:38 +0900

    [#1] admin css 전역 적용 문제 해결

--- src/pages/admin/Admin.tsx
+++ src/pages/admin/Admin.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import style from "./Admin.module.scss";
+import { useState } from 'react'
+import style from './Admin.module.scss'
 
 const Admin = () => {
   const [data, setData] = useState({
@@ -7,52 +7,51 @@ const Admin = () => {
     data: [
       {
         id: 1,
-        name: "아무개",
-        email: "neo@zillinks.com",
-        employeeNumber: "#1234",
+        name: '아무개',
+        email: 'neo@zillinks.com',
+        employeeNumber: '#1234',
         restAnnual: 23,
-        workDay: 10,
+        workDay: 10
       },
       {
         id: 2,
-        name: "아무개",
-        email: "neo@zillinks.com",
-        employeeNumber: "#5678",
+        name: '아무개',
+        email: 'neo@zillinks.com',
+        employeeNumber: '#5678',
         restAnnual: 20,
-        workDay: 9,
-      },
-    ],
-  });
+        workDay: 9
+      }
+    ]
+  })
 
   const handleDelete = (id) => {
-    console.log(`삭제 버튼 클릭: ${id}`);
-  };
+    console.log(`삭제 버튼 클릭: ${id}`)
+  }
 
   return (
     <section className={style.container}>
       <div className={style.contentWrapper}>
         <table className={style.table}>
           <thead>
-            <tr>
-              <th>사원명</th>
-              <th>이메일</th>
-              <th>잔여 연차/당직 근무일 수</th>
-              <th>빈칸</th>
+            <tr className={style.tr}>
+              <th className={style.th}>사원명</th>
+              <th className={style.th}>이메일</th>
+              <th className={style.th}>잔여 연차/당직 근무일 수</th>
+              <th className={style.th}>빈칸</th>
             </tr>
           </thead>
           <tbody>
             {data.data.map((employee) => (
-              <tr key={employee.id}>
-                <td>{employee.name} {employee.employeeNumber}</td>
-                <td>{employee.email}</td>
+              <tr key={employee.id} className={style.tr}>
                 <td>
+                  {employee.name} {employee.employeeNumber}
+                </td>
+                <td>{employee.email}</td>
+                <td className={style.td}>
                   {employee.restAnnual}/{employee.workDay}
                 </td>
-                <td>
-                  <button
-                    className={style.delete}
-                    onClick={() => handleDelete(employee.id)}
-                  >
+                <td className={style.td}>
+                  <button className={style.delete} onClick={() => handleDelete(employee.id)}>
                     삭제
                   </button>
                 </td>
@@ -62,7 +61,7 @@ const Admin = () => {
         </table>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default Admin;
+export default Admin
