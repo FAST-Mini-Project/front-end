4051ae586feface298277cd97ae6788f845debb5
ruddnjs3769 <84277185+ruddnjs3769@users.noreply.github.com>
2023-07-27 16:30:10 +0900

    [#8] feat: api types 정리

---
+++ src/types/AccessTypes.ts
@@ -0,0 +1,54 @@
+// Req : Request
+// Res : Response
+
+//로그인
+// /api/login
+export interface LoginReq {
+  email: string
+  password: string
+}
+
+// interface LoginRes {
+//   statusCode: number
+//   data: LoginResData
+// }
+
+export interface LoginResData {
+  user: User
+  token: string
+}
+
+// LoginRes.data
+export interface User {
+  email: string
+  name: string
+  employNumber: string
+  role: 'ROLE_ADMIN' | 'ROLE_USER'
+}
+
+// 회원가입
+// /api/signup
+export interface SignupReq {
+  email: string // 사용자 아이디 (필수!)
+  name: string // 사용자 이름, 20자 이하 (필수!)
+  password: string // 사용자 비밀번호, 8자 이상 (필수!)
+  employeeNumber: string // 사원번호(uuid) 생성한 uuid 값 그대로!
+}
+
+// interface SignupRes {
+//   statusCode: number // 201 Created
+//   data: Message
+// }
+
+// signupRes.data
+export interface Message {
+  message: string
+}
+// 실패 시 statusCode: 400, message: '이미 존재하는 이메일입니다.'
+
+// 로그아웃
+//POST /api/logout
+// \ -H 'Authorization: "asjldhaslkjdhaslkjdhalskjdhalskj"'
+// req 없음
+
+// res 200 OK
