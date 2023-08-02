bfc27ba4f1d1dd4f2c6f3ca058194e5991efea94
ruddnjs3769 <84277185+ruddnjs3769@users.noreply.github.com>
2023-08-02 20:25:21 +0900

    [#23] feat: Login, Signup api 연동

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,26 +1,7 @@
 import style from './Main.module.scss'
 import CalendarForm from '@/components/main/CalendarForm'
 // import { signupApi, loginApi, logoutApi } from '@/api/user'
-// import { setCookie, getCookie } from '@/utils/cookie'
-// import { v4 } from 'uuid'
 const Main = () => {
-  // const handleLogout = () => {
-  //   logoutApi(getCookie('token')).then((res) => {
-  //     alert(res?.message)
-  //   })
-  // }
-  // const handleSignUp = () => {
-  //   signupApi({ email: 'frontfront@test.com', name: '김프론트', password: '123456789', employeeNumber: v4() })
-  // }
-  // const handleLogin = () => {
-  //   loginApi({ email: 'frontfront@test.com', password: '123456789' }).then((res) => {
-  //     console.log(res)
-  //     if (res) {
-  //       setCookie('token', res.token)
-  //       console.log(getCookie('token'))
-  //     }
-  //   })
-  // }
   return (
     <div className={style.main}>
       <div className={style.content}>
