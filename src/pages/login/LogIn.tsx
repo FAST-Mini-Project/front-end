b1187becbaf826186d6709ff1be48eb502b787f6
ruddnjs3769 <84277185+ruddnjs3769@users.noreply.github.com>
2023-08-04 16:25:36 +0900

    [#49] feat: 로그인/회원가입 유효성 검사

--- src/pages/login/LogIn.tsx
+++ src/pages/login/LogIn.tsx
@@ -3,12 +3,21 @@ import { useNavigate, Link } from 'react-router-dom'
 import style from './LogIn.module.scss'
 import { loginApi } from '@/api/user'
 import { setCookie, getCookie } from '@/utils/cookie'
+import { Tooltip } from 'antd'
+import { emailRegex, passwordRegex } from '@/utils/constants/regex'
 
 const LogIn = () => {
   const navigate = useNavigate()
 
   const [loginEmail, setLoginEmail] = useState('')
   const [loginPassword, setLoginPassword] = useState('')
+
+  //유효성 검사
+  const [emailValidateText, setEmailValidateText] = useState('')
+  const [passwordValidateText, setPasswordValidateText] = useState('')
+  const [emailFocused, setEmailFocused] = useState(false)
+  const [passwordFocused, setPasswordFocused] = useState(false)
+  const [loginButtonDisabled, setLoginButtonDisabled] = useState<boolean[]>([false, false])
 
   const handleLogin = async (e: React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
@@ -40,25 +49,76 @@ const LogIn = () => {
     }
   }, [])
 
+  useEffect(() => {
+    if (loginEmail === '') {
+      setEmailValidateText('🙂이메일을 입력해주세요.')
+    } else {
+      if (emailRegex.test(loginEmail)) {
+        setEmailValidateText('✅올바른 이메일 형식입니다.')
+        const updatedLoginButtonDisabled = [...loginButtonDisabled]
+        updatedLoginButtonDisabled[0] = true
+        setLoginButtonDisabled(updatedLoginButtonDisabled)
+      } else {
+        setEmailValidateText('❌이메일 형식이 올바르지 않습니다.')
+        const updatedLoginButtonDisabled = [...loginButtonDisabled]
+        updatedLoginButtonDisabled[0] = false
+        setLoginButtonDisabled(updatedLoginButtonDisabled)
+      }
+    }
+  }, [loginEmail])
+
+  useEffect(() => {
+    if (loginPassword === '') {
+      setPasswordValidateText('🙂비밀번호를 입력해주세요.')
+    } else {
+      if (passwordRegex.test(loginPassword)) {
+        setPasswordValidateText('✅올바른 비밀번호 형식입니다.')
+        const updatedLoginButtonDisabled = [...loginButtonDisabled]
+        updatedLoginButtonDisabled[1] = true
+        setLoginButtonDisabled(updatedLoginButtonDisabled)
+      } else {
+        setPasswordValidateText('❌8자 이상의 비밀번호를 작성해주세요')
+        const updatedLoginButtonDisabled = [...loginButtonDisabled]
+        updatedLoginButtonDisabled[1] = false
+        setLoginButtonDisabled(updatedLoginButtonDisabled)
+      }
+    }
+  }, [loginPassword])
+
   return (
     <form className={style.container} onSubmit={handleLogin}>
       <div className={style.box}>
         <h1 className={style.title}>로그인</h1>
-        <input
-          className={style.input}
-          type="email"
-          placeholder="이메일 입력"
-          onChange={(e) => setLoginEmail(e.target.value)}
-          required
-        />
-        <input
-          className={style.input}
-          type="password"
-          placeholder="비밀번호 입력"
-          onChange={(e) => setLoginPassword(e.target.value)}
-          required
-        />
-        <button className={style.loginButton} onClick={handleLogin} type="submit">
+        <Tooltip title={emailValidateText} open={emailFocused} placement="right">
+          <input
+            className={style.input}
+            type="email"
+            placeholder="이메일 입력"
+            onChange={(e) => setLoginEmail(e.target.value)}
+            onFocus={() => setEmailFocused(true)}
+            onBlur={() => setEmailFocused(false)}
+            required
+          />
+        </Tooltip>
+        <Tooltip title={passwordValidateText} open={passwordFocused} placement="right">
+          <input
+            className={style.input}
+            type="password"
+            placeholder="비밀번호 입력"
+            onChange={(e) => setLoginPassword(e.target.value)}
+            onFocus={() => setPasswordFocused(true)}
+            onBlur={() => setPasswordFocused(false)}
+            required
+          />
+        </Tooltip>
+        <button
+          className={`${style.loginButton} ${
+            loginButtonDisabled.every((check) => check === true) ? '' : style.disabled
+          }`}
+          onClick={handleLogin}
+          type="submit"
+          disabled={loginButtonDisabled.every((check) => check === true) ? false : true}
+        >
           로그인
         </button>
         <Link className={style.signupLink} to="/signup">
