1217d33e9f753b61946ab05fedd2693b2cd53f34
Geumyeop Yoon <yoongy3333@gmail.com>
2023-08-07 03:26:57 +0900

    [#45] feat: 잔여 연차 갯수 위치 및 기능 변경, MonthRange 기능 변경

--- src/components/mypage/MenuTab.tsx
+++ src/components/mypage/MenuTab.tsx
@@ -1,20 +1,16 @@
 import { NavLink } from 'react-router-dom'
-import { annuals } from '@/types/MypageTypes'
-import RemainingAnnual from '@/components/mypage/RemainingAnnual'
 import styles from './MenuTab.module.scss'
 
 // MenuTab에서 사용할 props 타입 정의
 interface MenuTabProps {
   activeTab: 'annual' | 'duty' | 'info'
   handleTabClick: (tab: 'annual' | 'duty' | 'info') => void
-  // 잔여 연차 갯수를 조회하는 RemainingAnnual 컴포넌트에 전달할 Data
-  annualData: annuals[]
 }
 
-// Mypage 상단에 출력될 MenuTab Component
-const MenuTab: React.FC<MenuTabProps> = ({ activeTab, handleTabClick, annualData }) => {
+// MyPage 상단에 출력될 MenuTab Component
+const MenuTab: React.FC<MenuTabProps> = ({ activeTab, handleTabClick }) => {
   return (
-    <nav className={styles.menu__wrapper}>
+    <nav className={styles.menu__container}>
       <ul className={styles.menu__tab}>
         <li>
           <NavLink
@@ -43,8 +39,6 @@ const MenuTab: React.FC<MenuTabProps> = ({ activeTab, handleTabClick, annualData
             비밀번호 수정
           </NavLink>
         </li>
-        {/* '연차 조회' 탭 활성화 시 RemainingAnnual 출력 */}
-        {activeTab === 'annual' && <RemainingAnnual annualData={annualData} />}
       </ul>
     </nav>
   )
--- src/components/mypage/MonthRange.tsx
+++ src/components/mypage/MonthRange.tsx
@@ -5,18 +5,44 @@ import styles from './MonthRange.module.scss'
 interface MonthRangeProps {
   selectedYear: number
   selectedMonth: number
-  onPrevMonthClick: () => void
-  onNextMonthClick: () => void
   onMonthChange: (year: number, month: number) => void
 }
 
 // 연차 및 당직 리스트에서 년도와 월을 선택하여 조회할 수 있는 MonthRange component
-const MonthRange: React.FC<MonthRangeProps> = ({ selectedYear, selectedMonth, onPrevMonthClick, onNextMonthClick }) => {
+const MonthRange: React.FC<MonthRangeProps> = ({ selectedYear, selectedMonth, onMonthChange }) => {
+  const handleYearChange = (year: number, month: number, direction: 'prev' | 'next') => {
+    // HiChevronLeft Icon을 클릭 시 년도와 월을 알맞게 변경
+    if (direction === 'prev') {
+      if (month === 1) {
+        onMonthChange(year - 1, 12)
+      } else {
+        onMonthChange(year, month - 1)
+      }
+    } else {
+      // HiChevronRight Icon을 클릭 시 년도와 월을 알맞게 변경
+      if (month === 12) {
+        onMonthChange(year + 1, 1)
+      } else {
+        onMonthChange(year, month + 1)
+      }
+    }
+  }
+
+  // HiChevronLeft Icon을 클릭 시 수행되는 함수
+  const handlePrevMonthClick = () => {
+    handleYearChange(selectedYear, selectedMonth, 'prev')
+  }
+
+  // HiChevronRight Icon을 클릭 시 수행되는 함수
+  const handleNextMonthClick = () => {
+    handleYearChange(selectedYear, selectedMonth, 'next')
+  }
+
   return (
     <div className={styles.range__container}>
-      <HiChevronLeft onClick={onPrevMonthClick} className={styles.button__left} />
+      <HiChevronLeft onClick={handlePrevMonthClick} className={styles.button__left} />
       <p>{`${selectedYear}년 ${selectedMonth}월`}</p>
-      <HiChevronRight onClick={onNextMonthClick} className={styles.button__right} />
+      <HiChevronRight onClick={handleNextMonthClick} className={styles.button__right} />
     </div>
   )
 }
