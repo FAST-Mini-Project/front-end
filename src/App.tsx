2b39d2c944dcd1e9453fcd9833ef8f81fc3dc083
ruddnjs3769 <84277185+ruddnjs3769@users.noreply.github.com>
2023-08-07 17:56:24 +0900

    [#64] feat: codeSplit

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,103 @@
-import './App.module.scss'
 import { Route, Routes } from 'react-router-dom'
-import MainLayout from './layout/MainLayout'
-import Main from './pages/main/Main'
-import MyPage from './pages/mypage/Mypage'
-import AdminEmployee from './pages/admin/AdminEmployee'
-import AdminDuty from './pages/admin/AdminDuty'
-import AdminAnnual from './pages/admin/AdminAnnual'
-import AdminLayout from './layout/AdminLayout'
-import LogIn from './pages/login/LogIn'
-import SignUp from './pages/login/SignUp'
+
+//loadble
+import loadable from '@loadable/component'
+//spinner
+import { Spin } from 'antd'
 
 import { UserPrivateRoute } from './utils/PrivateRoute'
 import AdminPrivateRouteWrapper from '@/layout/AdminPrivateRouteWrapper'
 
+const MainLayout = loadable(() => import('@/layout/MainLayout'), {
+  fallback: (
+    <div className="spin">
+      <Spin size="large" />
+    </div>
+  )
+})
+const Main = loadable(() => import('@/pages/main/Main'), {
+  fallback: (
+    <div className="spin">
+      <Spin size="large" />
+    </div>
+  )
+})
+const MyPage = loadable(() => import('@/pages/mypage/Mypage'), {
+  fallback: (
+    <div className="spin">
+      <Spin size="large" />
+    </div>
+  )
+})
+const AdminEmployee = loadable(() => import('@/pages/admin/AdminEmployee'), {
+  fallback: (
+    <div className="spin">
+      <Spin size="large" />
+    </div>
+  )
+})
+const AdminDuty = loadable(() => import('@/pages/admin/AdminDuty'), {
+  fallback: (
+    <div className="spin">
+      <Spin size="large" />
+    </div>
+  )
+})
+const AdminAnnual = loadable(() => import('@/pages/admin/AdminAnnual'), {
+  fallback: (
+    <div className="spin">
+      <Spin size="large" />
+    </div>
+  )
+})
+const AdminLayout = loadable(() => import('@/layout/AdminLayout'), {
+  fallback: (
+    <div className="spin">
+      <Spin size="large" />
+    </div>
+  )
+})
+const LogIn = loadable(() => import('@/pages/login/LogIn'), {
+  fallback: (
+    <div className="spin">
+      <Spin size="large" />
+    </div>
+  )
+})
+const SignUp = loadable(() => import('@/pages/login/SignUp'), {
+  fallback: (
+    <div className="spin">
+      <Spin size="large" style={{ fontSize: '64px' }} />
+    </div>
+  )
+})
+
 function App() {
   return (
-    <Routes>
-      {/* 일반 유저 페이지 */}
-      <Route path="/login" element={<LogIn />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route element={<UserPrivateRoute />}>
-        <Route path="/" element={<MainLayout />}>
-          <Route path="/" element={<Main />} />
-          <Route path="/mypage/*" element={<MyPage />} />
+    <>
+      <Routes>
+        {/* 일반 유저 페이지 */}
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route element={<UserPrivateRoute />}>
+          <Route path="/" element={<MainLayout />}>
+            <Route path="/" element={<Main />} />
+            <Route path="/mypage/*" element={<MyPage />} />
+          </Route>
         </Route>
-      </Route>
-      {/* 관리자 페이지  */}
-      <Route element={<AdminPrivateRouteWrapper />}>
-        <Route path="/admin" element={<AdminLayout />}>
-          <Route path="/admin/employee" element={<AdminEmployee />} />
-          <Route path="/admin/duty" element={<AdminDuty />} />
-          <Route path="/admin/annual" element={<AdminAnnual />} />
+        {/* 관리자 페이지  */}
+        <Route element={<AdminPrivateRouteWrapper />}>
+          <Route path="/admin" element={<AdminLayout />}>
+            <Route path="/admin/employee" element={<AdminEmployee />} />
+            <Route path="/admin/duty" element={<AdminDuty />} />
+            <Route path="/admin/annual" element={<AdminAnnual />} />
+          </Route>
         </Route>
-      </Route>
-    </Routes>
+      </Routes>
+      <div className="spin">
+        <Spin size="large" style={{ fontSize: '64px' }} />
+      </div>
+    </>
   )
 }
 
