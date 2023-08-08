3b36660a30b5e6dd7fe0e11775da96d0883bc36a
hwanginseung <hwanginseung@naver.com>
2023-08-08 22:01:26 +0900

    feat:main,admin 사원명 너무 길면 잘라냄

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App.tsx";
@@ -6,11 +5,9 @@ import { BrowserRouter } from "react-router-dom";
 import { CookiesProvider } from "react-cookie";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
     <CookiesProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </CookiesProvider>
-  </React.StrictMode>
 );
